14ba4d674b61f6922168c5ecde13d0f8d4d13230
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-07-25 18:13:39 -0700

    mini

--- ecoforecasting/named_model.py
+++ ecoforecasting/named_model.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+from darts.models.forecasting.forecasting_model import ForecastingModel 
+# the base forecasting model class 
+# used to specify type, but only decendents have functionality
+
 @dataclass
 class named_model:
 	model: ForecastingModel
