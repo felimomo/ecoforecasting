70c89ab27ae3a06bbde52d73012143202144999b
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-07-25 18:36:43 -0700

    bugs

--- tests/tuning_test.py
+++ tests/tuning_test.py
@@ -51,7 +51,7 @@ MODEL_KWARGS = {"add_encoders": ADD_ENCODERS}
 FIT_KWARGS = {
 	"past_covariates": PAST_COVARIATES, 
 	"max_samples_per_ts": 1000, 
-	"num_workers": 4
+	"num_loader_workers": 4,
 }
 
 # initializing objects
